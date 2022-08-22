99e92839be91ef8de37f2edfee8ef890901aac84
anuanchen <79898167+anuanchen@users.noreply.github.com>
2022-08-22 14:45:45 +0800

    Update main.py

--- main.py
+++ main.py
@@ -25,11 +25,11 @@ def get_weather():
   return weather['weather'], math.floor(weather['temp'])
 
 def get_count():
-  delta = today - datetime.strptime(start_date, "%Y-%m-%d")
+  delta = today - datetime.strptime(start_date, "%Y/%m/%d")
   return delta.days
 
 def get_birthday():
-  next = datetime.strptime(str(date.today().year) + "-" + birthday, "%Y-%m-%d")
+  next = datetime.strptime(str(date.today().year) + "-" + birthday, "%Y/%m/%d")
   if next < datetime.now():
     next = next.replace(year=next.year + 1)
   return (next - today).days
