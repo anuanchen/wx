5c16faf0a96f02a321bfb94befe907998ccb6fac
anuanchen <79898167+anuanchen@users.noreply.github.com>
2022-08-22 15:37:18 +0800

    Update main.py

--- main.py
+++ main.py
@@ -35,7 +35,7 @@ def get_birthday():
   return (next - today).days
 
 def get_words():
-  words = requests.get("https://api.shadiao.pro/chp")
+  words = requests.get("https://api.aa1.cn/api/api-wenan-shenhuifu/index.php")
   if words.status_code != 200:
     return get_words()
   return words.json()['data']['text']
