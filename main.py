6be208629f7ef4c22a705a9057292a7e93187ba7
anuanchen <79898167+anuanchen@users.noreply.github.com>
2022-08-22 16:20:28 +0800

    Update main.py

--- main.py
+++ main.py
@@ -34,20 +34,11 @@ def get_access_token():
 def get_birthday(birthday, year, today):
     birthday_year = birthday.split("-")[0]
     # 判断是否为农历生日
-    if birthday_year[0] == "r":
-        r_mouth = int(birthday.split("-")[1])
-        r_day = int(birthday.split("-")[2])
+    # 获取国历生日的今年对应月和日
+    birthday_month = int(birthday.split("-")[1])
+    birthday_day = int(birthday.split("-")[2])
         # 今年生日
-        birthday = ZhDate(year, r_mouth, r_day).to_datetime().date()
-        year_date = birthday
-
-
-    else:
-        # 获取国历生日的今年对应月和日
-        birthday_month = int(birthday.split("-")[1])
-        birthday_day = int(birthday.split("-")[2])
-        # 今年生日
-        year_date = date(year, birthday_month, birthday_day)
+    year_date = date(year, birthday_month, birthday_day)
     # 计算生日年份，如果还没过，按当年减，如果过了需要+1
     if today > year_date:
         if birthday_year[0] == "r":
